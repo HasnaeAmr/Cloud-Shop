c9b5609d19c634dac46ec49fe651e7f7dd494ee0
Hasna Amarmach <amarmachhasna22@gmail.com>
2025-01-25 04:03:42 +0100

    Application Setup

---
+++ SHS-Ecommerce/src/main/java/com/shs/SHS/Ecommerce/ShsEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.shs.SHS.Ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShsEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShsEcommerceApplication.class, args);
+	}
+
+}
---
+++ SHS-Ecommerce/src/test/java/com/shs/SHS/Ecommerce/ShsEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shs.SHS.Ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShsEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
