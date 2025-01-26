75e41d5adf28a27d21368465299b95720233e6a7
Hasna Amarmach <amarmachhasna22@gmail.com>
2025-01-26 03:13:36 +0100

    Add global exception handling and custom exceptions

---
+++ SHS-Ecommerce/src/main/java/com/shs/SHS/Ecommerce/exception/NoFoundException.java
@@ -0,0 +1,7 @@
+package com.shs.SHS.Ecommerce.exception;
+
+public class NoFoundException extends RuntimeException {
+    public NoFoundException(String message) {
+        super(message);
+    }
+}
