337ea3d215fbfcf93dc2711ca27434d06b916a80
Hasna Amarmach <amarmachhasna22@gmail.com>
2025-01-26 02:29:35 +0100

    Create a Generic Response Class

---
+++ SHS-Ecommerce/src/main/java/com/shs/SHS/Ecommerce/dto/Response.java
@@ -0,0 +1,40 @@
+package com.shs.SHS.Ecommerce.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Response {
+
+    private int status;
+    private String message;
+    private final LocalDateTime timestamp = LocalDateTime.now();
+    private String token;
+    private String role;
+    private String expirationTime;
+    private long totalElement;
+    
+    private AddressDto address;
+
+    private UserDto user;
+    private List<UserDto> users;
+
+    private CategoryDto category;
+    private List<CategoryDto> categories;
+
+    private ProductDto product;
+    private List<ProductDto> products;
+
+    private OrderDto order;
+    private List<OrderDto> orders;
+
+    private OrderItemDto orderItem;
+    private List<OrderItemDto> orderItems;
+}
