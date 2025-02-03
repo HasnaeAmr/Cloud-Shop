b1cfe809b8b54b132ae1df9582a7fe23b44ea4b1
Hasna Amarmach <amarmachhasna22@gmail.com>
2025-02-03 23:53:31 +0100

    Add JWT token generation methods in JwtUtils

---
+++ SHS-Ecommerce/src/main/java/com/shs/SHS/Ecommerce/security/JwtUtils.java
@@ -0,0 +1,41 @@
+package com.shs.SHS.Ecommerce.security;
+
+import com.shs.SHS.Ecommerce.entity.User;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import io.jsonwebtoken.Jwts;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Service
+@Slf4j
+public class JwtUtils {
+
+    private static final long EXPIRATION_TIME_IN_MILLISEC = 1000L * 60L * 24L * 30L * 6L; // 6 months
+    private SecretKey secretKey;
+
+    @Value("${secreteJwtString}")
+    private String secreteJwtString; // value for application properties (32 chars or long)
+
+    @PostConstruct
+    private void init(){
+        byte[] keyBytes = secreteJwtString.getBytes(StandardCharsets.UTF_8);
+        this.secretKey = new SecretKeySpec(keyBytes, "HmacSHA256");
+    }
+
+    public String generateToken(User user){
+        String username = user.getEmail();
+        return generateToken(username);
+    }
+
+    public String generateToken(String username){
+        return Jwts.builder().subject(username).issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME_IN_MILLISEC))
+                .signWith(secretKey).compact();
+    }
+}
