ae8bbee1d2f91b0b95be93d76020ce3e5a787592
Hasna Amarmach <amarmachhasna22@gmail.com>
2025-02-07 23:56:41 +0100

    Add JWT validation and authentication setup in request processing

--- SHS-Ecommerce/src/main/java/com/shs/SHS/Ecommerce/security/JwtAuthFilter.java
+++ SHS-Ecommerce/src/main/java/com/shs/SHS/Ecommerce/security/JwtAuthFilter.java
@@ -6,7 +6,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -21,6 +25,20 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = getTokenFromRequest(request);
+
+        if(token != null){
+            String username = jwtUtils.getUsernameFromToken(token);
+            UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
+            if(StringUtils.hasText(username) && jwtUtils.isTokenValid(token, userDetails)) {
+                log.info("Valid JWT for : {}", token);
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                
+            }
+        }
+
+        filterChain.doFilter(request,response);
+
     }
 
     private String getTokenFromRequest(HttpServletRequest request) {
